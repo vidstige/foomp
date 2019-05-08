921cf03664f9e14660b461d070ac4ba2b558163e
Samuel Carlsson <samuel.carlsson@gmail.com>
2019-05-08 20:36:58 +0200

    base code

---
+++ foomp.py
@@ -0,0 +1,28 @@
+import cairo
+import math
+
+TWITTER = 506, 253
+TAU = 2 * math.pi
+
+
+def clear(target: cairo.ImageSurface, color=(1, 1, 1)) -> None:
+    r, g, b = color
+    ctx = cairo.Context(target)
+    ctx.rectangle(0, 0, target.get_width(), target.get_height())
+    ctx.set_source_rgb(r, g, b)
+    ctx.fill()
+
+
+def animate(f, dt, draw, step=None, resolution=TWITTER):
+    width, height = resolution
+    surface = cairo.ImageSurface(cairo.Format.RGB24, width, height)
+    #n = 100
+    #for i in range(0, n):
+    t = 0
+    while True:
+        t += dt
+        clear(surface, color=(0, 0, 0))
+        if step:
+            step(dt)
+        draw(surface, resolution)
+        f.write(surface.get_data())
