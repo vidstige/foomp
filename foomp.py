f46cd570d081c48a78f955decfcfaa114190d336
Samuel Carlsson <samuel.carlsson@gmail.com>
2019-05-13 22:20:19 +0200

    support for stop time

--- foomp.py
+++ foomp.py
@@ -13,13 +13,13 @@ def clear(target: cairo.ImageSurface, color=(1, 1, 1)) -> None:
     ctx.fill()
 
 
-def animate(f, dt, draw, step=None, resolution=TWITTER):
+def animate(f, dt, draw, step=None, resolution=TWITTER, until=None):
     width, height = resolution
     surface = cairo.ImageSurface(cairo.Format.RGB24, width, height)
     #n = 100
     #for i in range(0, n):
     t = 0
-    while True:
+    while not until or t < until:
         t += dt
         clear(surface, color=(0, 0, 0))
         if step:
