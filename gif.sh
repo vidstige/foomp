434d725f5025c6b7c212f886377a134b3e43d5d4
Samuel Carlsson <samuel.carlsson@gmail.com>
2019-06-30 22:00:11 +0200

    framerate

--- gif.sh
+++ gif.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 VIDEO_SIZE=506x253
-ffmpeg -f rawvideo -pixel_format rgb32 -video_size $VIDEO_SIZE -i output.raw -f gif output.gif
+ffmpeg -f rawvideo -pixel_format rgb32 -video_size $VIDEO_SIZE -framerate 30 -i output.raw -f gif output.gif
